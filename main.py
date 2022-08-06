af819e5c7ec4a52b2a110bf4974b2cd3d94aa9e1
th8887 <th8887@bs.amalnet.k12.il>
2022-08-06 12:47:56 +0300

    ex001 completed

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+age = int(input("Enter your age\n"))
+
+if age == 18 :
+    print("Congratulations")
+elif age<18:
+    print("You are so young")
+else:
+    print("We love old people")
